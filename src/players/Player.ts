5910d0707313e4e2ddd4a44cbd1d139bd13fea24
xFallen54x <65183965+xFallen54x@users.noreply.github.com>
2023-10-06 21:43:15 -0400

    Update to Player Class

--- src/players/Player.ts
+++ src/players/Player.ts
@@ -1,6 +1,12 @@
-// Import system, world, and Player from "@minecraft/server" module
-import { system, world, Player as IPlayer } from "@minecraft/server";
-
+// Define imports
+import {
+  system,
+  world,
+  Player as IPlayer,
+  EntityHealthComponent,
+  EntityComponent,
+  TitleDisplayOptions,
+} from "@minecraft/server";
 import { ModalForm, MessageForm, ActionForm } from "../forms/index.js";
 
 // Define a Player class
@@ -24,7 +30,8 @@ export class Player {
 
   /**
    * Constructor for the Player class
-   * @param {IPlayer} player - The player object from the "@minecraft/server" module
+   *
+   * @param player - The player object from the "@minecraft/server" module
    */
   constructor(player: IPlayer) {
     this._IPlayer = player;
@@ -33,9 +40,27 @@ export class Player {
   }
 
   /**
+   * Destroys the instance of the Player.
+   *
+   * @param reason - The reason for destroying the instance. Default value is "Instance of Player was destroyed."
+   */
+  public destroy(reason = "Instance of Player was destroyed.") {
+    this._IPlayer.runCommand(`kick "${this.displayName}" ${reason}`);
+  }
+
+  /**
+   * Retrieves the ID of the player.
+   *
+   * @return The ID of the player.
+   */
+  public getId(): string {
+    return this._IPlayer.id;
+  }
+
+  /**
    * Get the position of the player
-   * 
-   * @returns {Object} The location of the player
+   *
+   * @returns The location of the player
    */
   getPos(): { x: number; y: number; z: number } {
     return this._IPlayer.location;
@@ -43,9 +68,9 @@ export class Player {
 
   /**
    * Set the display name of the player
-   * 
-   * @param {string} name - The new display name for the player
-   * @param {string} [color] - The color code for the display name (optional)
+   *
+   * @param name - The new display name for the player
+   * @param color - The color code for the display name (optional)
    */
   public setDisplayName(name: string, color: string) {
     this.displayName = color ? `${color}${name}` : name;
@@ -55,7 +80,7 @@ export class Player {
   /**
    * Returns the IPlayer object.
    *
-   * @return {IPlayer} The IPlayer object.
+   * @returns The IPlayer object.
    */
   public getIPlayer(): IPlayer {
     return this._IPlayer;
@@ -63,8 +88,8 @@ export class Player {
 
   /**
    * Add a tag to the player
-   * 
-   * @param {string} tag - The tag to add to the player
+   *
+   * @param tag - The tag to add to the player
    */
   public addTag(tag: string) {
     if (typeof tag !== "string") {
@@ -75,8 +100,8 @@ export class Player {
 
   /**
    * Remove a tag from the player
-   * 
-   * @param {string} tag - The tag to remove from the player
+   *
+   * @param tag - The tag to remove from the player
    */
   public removeTag(tag: string) {
     if (typeof tag !== "string") {
@@ -87,9 +112,9 @@ export class Player {
 
   /**
    * Check if the player has a specific tag
-   * 
-   * @param {string} tag - The tag to check for
-   * @returns {boolean} True if the player has the tag, false otherwise
+   *
+   * @param tag - The tag to check for
+   * @returns True if the player has the tag, false otherwise
    */
   public hasTag(tag: string): boolean {
     if (typeof tag !== "string") {
@@ -100,8 +125,8 @@ export class Player {
 
   /**
    * Get all tags of the player
-   * 
-   * @returns {Array<string>} An array of all tags of the player
+   *
+   * @returns An array of all tags of the player
    */
   public getTags(): Array<string> {
     return this._IPlayer.getTags();
@@ -109,8 +134,8 @@ export class Player {
 
   /**
    * Add multiple tags to the player
-   * 
-   * @param {Array<string>} tags - An array of tags to add to the player
+   *
+   * @param tags - An array of tags to add to the player
    */
   public addTags(tags: Array<string>) {
     if (!Array.isArray(tags)) {
@@ -124,8 +149,8 @@ export class Player {
 
   /**
    * Remove multiple tags from the player
-   * 
-   * @param {Array<string>} tags - An array of tags to remove from the player
+   *
+   * @param tags - An array of tags to remove from the player
    */
   public removeTags(tags: Array<string>) {
     if (!Array.isArray(tags)) {
@@ -140,9 +165,9 @@ export class Player {
 
   /**
    * Check if the player has all specified tags
-   * 
-   * @param {Array<string>} tags - An array of tags to check for
-   * @returns {boolean} True if the player has all specified tags, false otherwise.
+   *
+   * @param tags - An array of tags to check for
+   * @returns True if the player has all specified tags, false otherwise.
    */
   public hasTags(tags: Array<string>): boolean {
     if (!Array.isArray(tags)) {
@@ -154,9 +179,9 @@ export class Player {
   /**
    * Teleport the player to a specific location.
    *
-   * @param {number} x - The x-coordinate of the location.
-   * @param {number} y - The y-coordinate of the location.
-   * @param {number} z - The z-coordinate of the location.
+   * @param x - The x-coordinate of the location.
+   * @param y - The y-coordinate of the location.
+   * @param z - The z-coordinate of the location.
    */
   public teleport(x: number, y: number, z: number) {
     if (
@@ -200,5 +225,71 @@ export class Player {
    */
   public createActionForm(): ActionForm {
     return new ActionForm(this);
+  }
+
+  /**
+   * Retrieves a component by name from the player.
+   *
+   * @param name - The name of the component to retrieve.
+   * @returns The retrieved component.
+   */
+  public getComponent(name: string): EntityComponent {
+    if (typeof name !== "string") {
+      throw new Error("Component name must be a string");
+    }
+    return this._IPlayer.getComponent(name);
+  }
+
+  /**
+   * Retrieves the health value of the entity.
+   *
+   * @returns The current health value of the entity.
+   */
+  public getHealth(): number {
+    const healthComponent = this.getComponent(
+      "health"
+    ) as EntityHealthComponent;
+    if (healthComponent) {
+      return healthComponent.currentValue;
+    }
+  }
+
+  /**
+   *  Set the player's health.
+   *
+   * @param health The health to set.
+   * If health is less than 0, the player will be killed.
+   * @returns True if the health was set successfully, false otherwise.
+   */
+  public setHealth(health: number): boolean {
+    if (typeof health !== "number") {
+      throw new Error("Health must be a number");
+    }
+    const healthComponent = this.getComponent(
+      "health"
+    ) as EntityHealthComponent;
+    if (healthComponent) {
+      healthComponent.setCurrentValue(health);
+    }
+    return true;
+  }
+
+  /**
+   * Sends a title to the player.
+   * @param message Message content to set.
+   * @param options Options for the title message.
+   */
+  public sendTitle(message: string): void {
+    const display = this._IPlayer.onScreenDisplay;
+    display.setTitle(message);
+  }
+
+  /**
+   * Sends a subtitle to the player.
+   * @param message Message content to set.
+   */
+  public sendSubtitle(message: string): void {
+    const display = this._IPlayer.onScreenDisplay;
+    display.updateSubtitle(message);
   }
 }
